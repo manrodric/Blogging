d3466446eb4c3987cbd655a87278454bb8c545f0
miguel.rodriguez <miguel.rodriguez@avantica.net>
2019-07-26 18:15:21 -0500

    download pdf browser

--- DynamicReportsWithJSFServletsAndCDI/src/main/java/com/ithinkisink/managedbean/DynamicReportsManagedBean.java
+++ DynamicReportsWithJSFServletsAndCDI/src/main/java/com/ithinkisink/managedbean/DynamicReportsManagedBean.java
@@ -1,27 +1,31 @@
 package com.ithinkisink.managedbean;
 
-import static net.sf.dynamicreports.report.builder.DynamicReports.report;
-
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
-import javax.servlet.ServletContext;
-
-import net.sf.dynamicreports.report.exception.DRException;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import javax.servlet.ServletOutputStream;
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletOutputStream;
+//import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 import com.ithinkisink.model.Constancia;
-import com.ithinkisink.model.InvoiceDetails;
-import com.ithinkisink.model.Item;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 /**
  * 
  * @author Belal
@@ -40,23 +44,23 @@ public class DynamicReportsManagedBean implements Serializable {
 	@PostConstruct
 	public void init() {
 //		invoiceName="Invoice.jrxml";
-		invoiceName="constancia.jrxml";
-		
-		List<Item> itemList = new ArrayList<>(Arrays.asList(
-				new Item("01", "Item 01 description", 1.1, 1), 
-				new Item("02", "Item 02 description", 2.2, 2),
-				new Item("03", "Item 03 description", 3.3, 3)));
-		beanCollectionDataSource = new JRBeanCollectionDataSource(itemList);
-		
-//		parameters = (new InvoiceDetails("INV#01", "Billing Company", "Billing Company Add", 
-//				"Billing Company State 1", "Billing Company State 2", 
-//				"Shipping Name", "Shipping Address", "Shipping State 1", "Shipping State 2", 1000)).toMap();
-		
-		parameters = (new Constancia("190713253239", "0000001107552", "13/07/2019 14:10:28", 
-				"11-01-2558990", "ERLITA CORDOVA FERNANDEZ", 
-				"Prueba de transferencia interbancarias en linea persona natural prueba de soporte cien caracteres",
-				"002-193-191153780123-12", "BANCO DE CREDITO DEL PERU", "PEREZ LOPEZ JUAN","Otro Cliente","Soles","S/ 100.00",
-				"S/2.50","S/7.00","S/0.45","S/0.00")).toMap();
+//		invoiceName="constancia.jrxml";
+//		
+//		List<Item> itemList = new ArrayList<>(Arrays.asList(
+//				new Item("01", "Item 01 description", 1.1, 1), 
+//				new Item("02", "Item 02 description", 2.2, 2),
+//				new Item("03", "Item 03 description", 3.3, 3)));
+//		beanCollectionDataSource = new JRBeanCollectionDataSource(itemList);
+//		
+////		parameters = (new InvoiceDetails("INV#01", "Billing Company", "Billing Company Add", 
+////				"Billing Company State 1", "Billing Company State 2", 
+////				"Shipping Name", "Shipping Address", "Shipping State 1", "Shipping State 2", 1000)).toMap();
+//		
+//		parameters = (new Constancia("190713253239", "0000001107552", "13/07/2019 14:10:28", 
+//				"11-01-2558990", "ERLITA CORDOVA FERNANDEZ", 
+//				"Prueba",
+//				"002-193-191153780123-12", "BANCO DE CREDITO DEL PERU", "PEREZ LOPEZ JUAN","Otro Cliente","Soles","S/ 100.00",
+//				"S/2.50","S/7.00","S/0.45","S/0.00")).toMap();
 		
 	}
 	
@@ -65,22 +69,22 @@ public class DynamicReportsManagedBean implements Serializable {
 	 * 
 	 * @return
 	 */
-	public OutputStream getOS(ServletContext context, OutputStream outputStream) {
-
-		parameters.put("IMAGE_PATH", context.getRealPath("/jasper/invoices"));
-
-		InputStream is = context.getResourceAsStream("/jasper/invoices/" + invoiceName);
-
-		try {
-			report().setTemplateDesign(is)
-					.setDataSource(beanCollectionDataSource)
-					.setParameters(parameters).toPdf(outputStream);
-		} catch (DRException e) {
-			e.printStackTrace();
-		}
-
-		return outputStream;
-	}
+//	public OutputStream getOS(ServletContext context, OutputStream outputStream) {
+//
+//		parameters.put("IMAGE_PATH", context.getRealPath("/jasper/invoices"));
+//
+//		InputStream is = context.getResourceAsStream("/jasper/invoices/" + invoiceName);
+//
+//		try {
+//			report().setTemplateDesign(is)
+//					.setDataSource(beanCollectionDataSource)
+//					.setParameters(parameters).toPdf(outputStream);
+//		} catch (DRException e) {
+//			e.printStackTrace();
+//		}
+//
+//		return outputStream;
+//	}
 
 	// setters and getters
 	public String getInvoiceName() {
@@ -107,4 +111,44 @@ public class DynamicReportsManagedBean implements Serializable {
 	public Map<String, Object> getParameters() {
 		return parameters;
 	}
+	
+	
+	public void exportarPDF(ActionEvent actionEvent) throws JRException, IOException{
+		
+		 List<Constancia> lstConstancia = new ArrayList<Constancia>();
+		 Constancia constancia = new Constancia("190713253239", "0000001107552", "13/07/2019 14:10:28", 
+					"11-01-2558990", "ERLITA CORDOVA FERNANDEZ", 
+					"Prueba",
+					"002-193-191153780123-12", "BANCO DE CREDITO DEL PERU", "PEREZ LOPEZ JUAN","Otro Cliente","Soles","S/ 100.00",
+					"S/2.50","S/7.00","S/0.45","S/0.00");
+		 lstConstancia.add(constancia);
+		 
+		
+		
+//		List<Item> itemList = new ArrayList<>(Arrays.asList(
+//				new Item("01", "Item 01 description", 1.1, 1), 
+//				new Item("02", "Item 02 description", 2.2, 2),
+//				new Item("03", "Item 03 description", 3.3, 3)));
+//		
+		
+		
+	
+		Map<String,Object> parametros= new HashMap<String,Object>();
+		parametros.put("prueba", "Miguel");
+		
+		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/jasper/invoices/constancia.jasper"));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(lstConstancia));
+		
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		response.addHeader("Content-disposition","attachment; filename=jsfReporte.pdf");
+		ServletOutputStream stream = response.getOutputStream();
+		
+		JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
+		
+		stream.flush();
+		stream.close();
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+	
+	
 }
